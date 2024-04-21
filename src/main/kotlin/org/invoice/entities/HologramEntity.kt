68784a523db5f4ab12f32892996c14e22b908ac8
Outspending <Dxrippy15@gmail.com>
2024-04-21 16:28:17 -0600

    Added `scale` to `HologramEntity`

--- src/main/kotlin/org/invoice/entities/HologramEntity.kt
+++ src/main/kotlin/org/invoice/entities/HologramEntity.kt
@@ -1,19 +1,21 @@
 package org.invoice.entities
 
 import net.kyori.adventure.text.Component
+import net.minestom.server.coordinate.Vec
 import net.minestom.server.entity.EntityType
 import net.minestom.server.entity.metadata.display.AbstractDisplayMeta
 import net.minestom.server.entity.metadata.display.TextDisplayMeta
 import org.invoice.mm
 
-class HologramEntity(private val text: Component) : NonTickingEntity(EntityType.TEXT_DISPLAY) {
-    constructor(text: String) : this(text.mm())
+class HologramEntity(private val text: Component, private val scale: Vec = Vec.ONE) : NonTickingEntity(EntityType.TEXT_DISPLAY) {
+    constructor(text: String, scale: Vec = Vec.ONE) : this(text.mm(), scale)
 
     init {
         editEntityMeta(TextDisplayMeta::class.java) { meta ->
             meta.text = text
             meta.billboardRenderConstraints = AbstractDisplayMeta.BillboardConstraints.CENTER
 
+            meta.scale = scale
             meta.isSeeThrough = true
             meta.backgroundColor = 0x0
         }
