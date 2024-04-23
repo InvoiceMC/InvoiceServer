3db3b66428a19b5f0a36ea9df786e65316cc726b
Azuyamat <discordazu@gmail.com>
2024-04-23 15:45:41 -0400

    fix: Allow Velocity proxy using args

--- src/main/kotlin/org/invoice/Main.kt
+++ src/main/kotlin/org/invoice/Main.kt
@@ -1,18 +1,26 @@
 package org.invoice
 
 import net.minestom.server.MinecraftServer
+import net.minestom.server.extras.velocity.VelocityProxy
 import java.io.IOException
 
 lateinit var server: InvoiceServer
 
 @Throws(IOException::class)
-fun main() {
+fun main(args: Array<String>) {
     // Create server object
     val minecraftServer = MinecraftServer.init()
     server = InvoiceServer(minecraftServer)
     server.start()
 
-    // VelocityProxy.enable("secret")
+
+    val velocitySecret = args.getOrNull(0)
+    if (velocitySecret == null) {
+        println("Velocity secret not found. Not enabling Velocity proxy.")
+    } else {
+        println("Velocity secret found. Enabling Velocity proxy.")
+        VelocityProxy.enable(velocitySecret)
+    }
 
     // Register all default events
     Events.setup()
